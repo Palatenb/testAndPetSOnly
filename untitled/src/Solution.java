22fbbaee29360a29ddfb30ba472e291c3d1c6f52
Palatenb <hutoram@yandex.ru>
2025-01-29 19:25:15 +0300

    Add new File!

--- untitled/src/Solution.java
+++ untitled/src/Solution.java
@@ -1,5 +1,4 @@
-public class StartModel {
-
+public class Solution {
 
     /*
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
@@ -8,9 +7,9 @@ Finish the solution so that it returns the sum of all the multiples of 3 or 5 be
 
 Note: If the number is a multiple of both 3 and 5, only count it once.
      */
-    public static void main(String[] args) {
 
+
+    public int solution(int number) {
+        //
     }
-
-
 }
